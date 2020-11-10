723841d4b53f56cde504241907b55f247af61c76
Nikolay <74046296+N2I4k6@users.noreply.github.com>
2020-11-10 13:16:24 +0300

    new Portfolio
    new Portfolio

---
+++ finishSite/js/script.js
@@ -0,0 +1,66 @@
+function left(){
+	let a,b,c;
+	
+	 a = document.getElementById("productionOne");
+	 a.style.background = "url(img/vansOne.png)";
+	 b = document.getElementById("productionTwo");
+	 b.style.background = "url(img/vansTwo.png)";
+	 c = document.getElementById("productionThree");
+	 c.style.background = "url(img/vansThree.png)";
+	
+}
+
+function right(){
+	let a,b,c;
+	
+	 a = document.getElementById("productionOne");
+	 a.style.background = "url(img/vansFour.png)";
+	 b = document.getElementById("productionTwo");
+	 b.style.background = "url(img/vansFive.png)";
+	 c = document.getElementById("productionThree");
+	 c.style.background = "url(img/vansSix.png)";
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/scriptOne.js
@@ -0,0 +1,90 @@
+
+function pinkThemea(){
+	let a,b;
+	for (let i=0; i<2; i++){
+	 a = document.getElementsByClassName("darkColorThemea")[i];
+	 a.style.backgroundColor = "#ff6699";
+	}
+	for (let i=0; i<2; i++){
+	a = document.getElementsByClassName("darkColorThemeaRamk")[i];
+	 a.style.border = "1px solid #ff6699";
+	}
+	for (let i=0; i<2; i++){
+	a = document.getElementsByClassName("darkColorThemeaLine")[i];
+	 a.style.borderBottom = "1px solid #ff6699";
+	}
+	for (let i=0; i<5; i++){
+	a = document.getElementsByClassName("colorThemeaLine")[i];
+	 a.style.borderBottom = "1px solid #ff99cc";
+	}
+	
+	for (let i=0; i<2; i++){
+	a = document.getElementsByClassName("colorThemea")[i];
+	 a.style.backgroundColor = "#ff99cc";
+	}
+	
+	b = document.getElementsByClassName("colorThemeaImg")[0];
+	 b.style.background = "url(../img/bigPhoto.png)";
+	
+	
+}
+
+
+function blueThemea(){
+	let a,b;
+	for (let i=0; i<2; i++){
+	 a = document.getElementsByClassName("darkColorThemea")[i];
+	 a.style.backgroundColor = "#0099ff";
+	}
+	for (let i=0; i<2; i++){
+	a = document.getElementsByClassName("darkColorThemeaRamk")[i];
+	 a.style.border = "1px solid #0099ff";
+	}
+	for (let i=0; i<2; i++){
+	a = document.getElementsByClassName("darkColorThemeaLine")[i];
+	 a.style.borderBottom = "1px solid #0099ff";
+	}
+	for (let i=0; i<5; i++){
+	a = document.getElementsByClassName("colorThemeaLine")[i];
+	 a.style.borderBottom = "1px solid #33adff";
+	}
+	
+	for (let i=0; i<2; i++){
+	a = document.getElementsByClassName("colorThemea")[i];
+	 a.style.backgroundColor = "#33adff";
+	}
+	
+	b = document.getElementsByClassName("colorThemeaImg")[0];
+	 b.style.background = "url(../img/bigPhotoTwo.png)";
+	
+	
+}
+
+function greenThemea(){
+	let a,b;
+	for (let i=0; i<2; i++){
+	 a = document.getElementsByClassName("darkColorThemea")[i];
+	 a.style.backgroundColor = "#408f04";
+	}
+	for (let i=0; i<2; i++){
+	a = document.getElementsByClassName("darkColorThemeaRamk")[i];
+	 a.style.border = "1px solid #408f04";
+	}
+	for (let i=0; i<2; i++){
+	a = document.getElementsByClassName("darkColorThemeaLine")[i];
+	 a.style.borderBottom = "1px solid #408f04";
+	}
+	for (let i=0; i<5; i++){
+	a = document.getElementsByClassName("colorThemeaLine")[i];
+	 a.style.borderBottom = "1px solid #57c206";
+	}
+	
+	for (let i=0; i<2; i++){
+	a = document.getElementsByClassName("colorThemea")[i];
+	 a.style.backgroundColor = "#57c206";
+	}
+	
+	b = document.getElementsByClassName("colorThemeaImg")[0];
+	 b.style.background = "url(../img/bigPhotoThree.jpg)";
+	
+}
